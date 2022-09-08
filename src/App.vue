6c3b87466e2108295dd2c6a0ad228edf60d59d93
Mehmet Ulga <mehmetulga@gmail.com>
2022-09-08 22:51:32 +0200

    v3

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="container">
-    <the-header></the-header>
-  </div>
+  <!-- <div class="container"> -->
+  <the-header></the-header>
+  <!-- </div> -->
+  <the-hero></the-hero>
+  <section-kids></section-kids>
 </template>
 
 <script>
+import SectionKids from "./components/SectionKids.vue";
 import TheHeader from "./components/TheHeader.vue";
+import TheHero from "./components/TheHero.vue";
 export default {
   name: "App",
-  components: { TheHeader },
+  components: { TheHeader, TheHero, SectionKids },
 };
 </script>
 
---
+++ src/components/SectionKids.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="hero-wrapper grid">
+        <div class="hero-image">
+          <img src="@/assets/image2.jpeg" alt="" />
+        </div>
+        <div class="hero-text">
+          <h1 class="hero-title-secondary">It’s Kids-Friendly Plants!</h1>
+          <p class="hero-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore.
+          </p>
+          <button class="btn btn-primary">Learn More</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+section {
+  padding: 10rem 0;
+}
+
+.grid {
+  /* grid-template-columns: 1fr 1fr; */
+  grid-template-columns: repeat(auto-fit, minmax(220px, auto));
+  gap: 9rem;
+}
+.hero-text {
+  margin: 28px 0;
+}
+</style>
