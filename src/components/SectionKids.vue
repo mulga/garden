85b70e2ba12cfc5c26072535d2fda06a8e4bf6e9
Mehmet Ulga <mehmetulga@gmail.com>
2022-09-14 08:55:44 +0200

    a

--- src/components/SectionKids.vue
+++ src/components/SectionKids.vue
@@ -1,20 +1,24 @@
 <template>
   <section>
-    <div class="container">
-      <div class="hero-wrapper grid">
-        <div class="hero-image">
-          <img src="@/assets/image2.jpeg" alt="" />
+    <!-- <div class="container"> -->
+    <div class="hero-wrapper grid">
+      <div class="hero-image">
+        <div class="green-area">
+          <div class="green-front"></div>
+          <div class="green-back"></div>
         </div>
-        <div class="hero-text">
-          <h1 class="hero-title-secondary">It’s Kids-Friendly Plants!</h1>
-          <p class="hero-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore.
-          </p>
-          <button class="btn btn-primary">Pre-Order Now</button>
-        </div>
+        <img src="@/assets/pot.png" alt="" />
+      </div>
+      <div class="hero-text">
+        <h1 class="hero-title-secondary">It’s Kids-Friendly Plants!</h1>
+        <p class="hero-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore.
+        </p>
+        <button class="btn btn-primary">Pre-Order Now</button>
       </div>
     </div>
+    <!-- </div> -->
   </section>
 </template>
 
@@ -23,6 +27,38 @@ export default {};
 </script>
 
 <style scoped>
+.hero-image {
+  position: relative;
+}
+.hero-image img {
+  position: absolute;
+  bottom: 0;
+  left: 10%;
+  width: 450px;
+}
+
+.green-area {
+  position: relative;
+}
+
+.green-back {
+  position: absolute;
+  top: -60px;
+  left: -150px;
+  width: 631px;
+  height: 465px;
+  background-color: #44ab9d;
+  border-top-right-radius: 50px;
+  z-index: -1;
+}
+
+.green-front {
+  width: 631px;
+  height: 465px;
+  background-color: #136b5f;
+  border-top-right-radius: 50px;
+}
+
 .grid {
   /* grid-template-columns: 1fr 1fr; */
   grid-template-columns: repeat(auto-fit, minmax(220px, auto));
